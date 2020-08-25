d5f5e020f7907e4da8091038f9bf382219211cbd
hai-nguyen1112 <hainguyen871112@gmail.com>
2020-08-25 17:17:23 -0400

    Add sass, add Home page

---
+++ src/components/Home/Home.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import CardMedia from '@material-ui/core/CardMedia';
+import { Link } from 'react-router-dom';
+import './Home.scss';
+
+const Home = () => {
+  return (
+    <>
+      <header className="App-header">
+        <CardMedia
+          component="img"
+          alt="Pikachu"
+          image="/pikachu-logo.gif"
+          title="Pikachu"
+          className="pikachu__image"
+        />
+        <p>
+          A Pokemon Searcher App using Material UI
+        </p>
+        <Link className="App-link" to="/pokedex">Go to Pokedex</Link>
+      </header>
+    </>
+  )
+};
+
+export default Home;
